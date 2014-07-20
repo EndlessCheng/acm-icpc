893987361f65008ce0ecd54e0149958cec7054aa
Σndless <loli.con@qq.com>
2014-07-20 22:18:29 +0800

    Create 557 - Burger (反向思考).cpp

---
+++ UVa/557 - Burger (反向思考).cpp
@@ -0,0 +1,22 @@
+#include<cstdio>
+double f[100005];
+
+void init()
+{
+	f[2] = 1;
+	for (int i = 2; i < 100000; i += 2)
+		f[i + 2] = f[i] * (i - 1) / i;
+}
+
+int main()
+{
+	init();
+	int n, t;
+	scanf("%d", &t);
+	while (t--)
+	{
+		scanf("%d", &n);
+		printf("%.4f\n", 1 - f[n]);
+	}
+	return 0;
+}
