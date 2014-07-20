fc1a0b0dc440c5818590b60fa124d2d22cee5a51
Σndless <loli.con@qq.com>
2014-07-20 16:18:54 +0800

    Update 448C - 分治解法.cpp

--- CF/448C - 分治解法.cpp
+++ CF/448C - 分治解法.cpp
@@ -9,7 +9,7 @@ int solve(int l, int r, int h) /*  @Tideline 超级好理解的分治算法   */
 	if (l == r) return 1;
 	int minh = *min_element(a + l, a + r + 1);
 	if (minh > h) return min(minh - h + solve(l, r, minh), r - l + 1);
-	// now m == h
+	// now minh == h
 	int cnt = 0, p = l;
 	for (int i = l; i <= r; ++i) if (a[i] == minh) cnt += solve(p, i - 1, h), p = i + 1;
 	cnt += solve(p, r, h);
