cd4a7fc91d7ebf3bc2dc7a2932766908d6560a15
EndlessCheng <loli.con@qq.com>
2014-07-30 15:50:27 +0800

    .

--- CF/407B.cpp
+++ CF/407B.cpp
@@ -10,10 +10,10 @@ int main()
 	int n;
 	cin >> n;
 	for (int i = 1; i <= n; i++) cin >> a[i];
-	dp[0] = -1;
-	dp[1] = 1;
+	dp[0] = -1, dp[1] = 1;
 	for (int i = 2; i <= n; i++)
-		dp[i] = (dp[i - 1] + 1 + dp[i - 1] - dp[a[i] - 1] + 1) % mod; // 前缀和
+		dp[i] = (dp[i - 1] + 1 + 1 + dp[i - 1] - dp[a[i] - 1]) % mod;
+        // 从前面的房间(dp[i-1])+进来(1)+出去(1)+从出去到再进来走过的门数(dp[i-1]-dp[a[i]-1])
 	cout << (dp[n] + 1 + mod) % mod;
 	return 0;
 }
