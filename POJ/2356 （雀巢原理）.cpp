99c449b3f15176870c6ba0431293945308c6ce70
Σndless <loli.con@qq.com>
2014-11-13 19:27:43 +0800

    Create 2356 （雀巢原理）.cpp

---
+++ POJ/2356 （雀巢原理）.cpp
@@ -0,0 +1,27 @@
+#include<cstdio>
+#include<cstring>
+
+int a[10001], pos[10001];
+
+int main() {
+	int n, i, sum, l, r;
+	scanf("%d", &n);
+	sum = 0;
+	memset(pos, -1, sizeof(pos));
+	pos[0] = 0;
+	for (i = 1; i <= n; ++i) {
+		scanf("%d", &a[i]);
+		sum = (sum + a[i]) % n;
+		if (pos[sum] == -1)
+			pos[sum] = i;
+		else {
+			l = pos[sum] + 1;
+			r = i;
+			break;
+		}
+	}
+	printf("%d\n", r - l);
+	for (i = l; i <= r; ++i)
+		printf("%d\n", a[i]);
+	return 0;
+}
